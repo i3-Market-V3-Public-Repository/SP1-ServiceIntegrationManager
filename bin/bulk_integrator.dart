b7bd3a1bd0ffded4fd15ff735b0ae2a025dcee73
Víctor Diví <victor.divi@upc.edu>
2021-03-19 16:38:44 +0100

    make bulk_integrator only use json files in spec folder

--- bin/bulk_integrator.dart
+++ bin/bulk_integrator.dart
@@ -12,13 +12,16 @@ void main(List<String> args) async {
 
 Future<void> runBulkIntegration(String backplanePath, String specsPath) async {
   final specsDir = Directory(specsPath);
-  final specs = specsDir.listSync(followLinks: false).whereType<File>().toList();
+  final specs = specsDir
+      .listSync(followLinks: false)
+      .whereType<File>()
+      .where((element) => extension(element.path) == '.json')
+      .toList();
   print('Integrating following services:');
   print('\t${specs.map((file) => basenameWithoutExtension(file.path)).join('\n\t')}');
   for (final spec in specs) {
     final serviceName = basenameWithoutExtension(spec.path);
     print('Integrating $serviceName');
-
     Directory('$backplanePath/integrated_services/sources').createSync(recursive: true);
     final fileName = '$backplanePath/integrated_services/$serviceName.json';
     final sourceName = '$backplanePath/integrated_services/sources/$serviceName.json';
