e24e684aa3170c5fdc61e1dcb0fe1988dec812eb
Víctor Diví <victor.divi@upc.edu>
2021-03-22 13:06:03 +0100

    Update secret management

--- bin/bulk_integrator.dart
+++ bin/bulk_integrator.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 import './integrate.dart';
 import 'package:path/path.dart';
+
+const chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890';
 
 void main(List<String> args) async {
   if (args.length != 2) {
@@ -11,7 +15,9 @@ void main(List<String> args) async {
 }
 
 Future<void> runBulkIntegration(String backplanePath, String specsPath) async {
-  File('$backplanePath/src/secrets.json').createSync();
+  final secretsFile = File('$backplanePath/.secrets.json')..createSync();
+  final secrets = jsonDecode(secretsFile.readAsStringSync()) as Map<String, String>;
+  final random = Random.secure();
   final specsDir = Directory(specsPath);
   final specs = specsDir
       .listSync(followLinks: false)
@@ -22,6 +28,10 @@ Future<void> runBulkIntegration(String backplanePath, String specsPath) async {
   print('\t${specs.map((file) => basenameWithoutExtension(file.path)).join('\n\t')}');
   for (final spec in specs) {
     final serviceName = basenameWithoutExtension(spec.path);
+    if (!secrets.containsKey(serviceName)) {
+      secrets[serviceName] =
+          String.fromCharCodes(Iterable.generate(32, (_) => chars.codeUnitAt(random.nextInt(chars.length))));
+    }
     print('Integrating $serviceName');
     Directory('$backplanePath/integrated_services/sources').createSync(recursive: true);
     final fileName = '$backplanePath/integrated_services/$serviceName.json';
@@ -32,6 +42,7 @@ Future<void> runBulkIntegration(String backplanePath, String specsPath) async {
     modifySpec(specFile);
     integrateService(backplanePath, serviceName, specFile);
   }
+  secretsFile.writeAsStringSync(JsonEncoder.withIndent('  ').convert(secrets));
 }
 
 void printHelp() {
